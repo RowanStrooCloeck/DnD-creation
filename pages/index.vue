d3cf40539e1eb2000e61a4b0dbf3cb441c7fe898
TRIMM\rowan.stroo.cloeck <rowan.stroo.cloeck@trimm.nl>
2021-12-01 14:48:31 +0100

    new layout using a table

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container pl-lg-5">
+  <div class="container pl-lg-5 mh-100">
 
-    <div class="row mt-4 selection-bar">
+    <div class="row pt-4 selection-bar">
 
-      <b-input-group class="col-sm-11 pl-0">
+      <b-input-group class="col-sm-11">
         <b-form-select class="col-sm-4" @change="getRaceBonuses(selectedRace)" v-model="selectedRace">
           <template #first>
             <b-form-select-option :value="null" disabled>Select a race</b-form-select-option>
@@ -28,81 +28,130 @@
           <b-form-select-option v-for="clas in classes" :value="clas.name">{{ clas.name }}</b-form-select-option>
         </b-form-select>
       </b-input-group>
-      <b-button class="col-sm-1" @click="reset">Reset</b-button>
-    </div>
-
-    <div class="row race-bonus">
-      <div v-if="raceBonuses" class="h3 col-sm-12 pl-0 pt-4">
-        Race Bonus points: {{ raceBonusPoints }}
-      </div>
-      <div class="col-sm-12 pl-0" v-if="raceBonuses">
-        <b-card-group deck class="col-sm-12">
-          <template v-for="stat in stats">
-            <template v-for="bonus in raceBonuses.bonusStats">
-              <div class="text-center mt-4 col-sm-2 px-0" v-if="bonus === stat.name">
-                <b-button @click="raceBonusUp(stat)"
-                          :disabled="raceBonusPoints <= 0 || stat.raceBonus >= 1"
-                          variant="success" size="lg">
-                  <b-icon-arrow-up-short></b-icon-arrow-up-short>
-                </b-button>
-                <b-card :header="stat.name" :key="stat.id" class="text-center my-2">
-                  <b-card-text class="h1">{{ stat.raceBonus }}</b-card-text>
-                </b-card>
-                <b-button @click="raceBonusDown(stat)" :disabled="stat.raceBonus === 0" variant="danger" size="lg">
-                  <b-icon-arrow-down-short></b-icon-arrow-down-short>
-                </b-button>
-              </div>
-            </template>
-
-          </template>
-        </b-card-group>
+      <div class="col-sm-1">
+        <b-button @click="reset">Reset</b-button>
       </div>
     </div>
 
-    <div class="row class-bonus">
-      <div v-if="classBonuses" class="h3 col-sm-12 pl-0 pt-4">
-        Class Bonus points: {{ classBonusPoints }}
-      </div>
-      <div v-if="classBonuses" class="col-sm-12 pl-0">
-        <b-card-group deck>
-          <template v-for="stat in stats">
-            <template v-for="bonus in classBonuses">
-              <div class="text-center mt-4 col-sm-2 px-0" v-if="bonus === stat.name">
-                <b-button @click="classBonusUp(stat)"
-                          :disabled="classBonusPoints <= 0 || stat.classBonus >= 2"
-                          variant="success" size="lg">
-                  <b-icon-arrow-up-short></b-icon-arrow-up-short>
-                </b-button>
-                <b-card :header="stat.name" :key="stat.id" class="text-center my-2">
-                  <b-card-text class="h1">{{ stat.classBonus }}</b-card-text>
-                </b-card>
-                <b-button @click="classBonusDown(stat)" :disabled="stat.classBonus === 0" variant="danger" size="lg">
-                  <b-icon-arrow-down-short></b-icon-arrow-down-short>
-                </b-button>
-              </div>
-            </template>
+    <div class="pt-1">
+      <b-table responsive :items="stats" :fields="fields">
 
-          </template>
-        </b-card-group>
-      </div>
+        <template #head(name)="data">
+          <span>Attribute</span>
+        </template>
+
+        <template #cell(name)="data">
+          <span class="h3">{{ data.item.name }}</span>
+        </template>
+
+        <template #head(number)="data">
+          PointBuy
+        </template>
+
+        <template #cell(number)="data">
+          <span class="h1">{{ data.item.number }}</span>
+        </template>
+
+        <template #head(numberBtn)="data">
+          <span>({{ pointBuy }})</span>
+        </template>
+
+        <template #cell(numberBtn)="data">
+          <div class="d-inline-grid text-center">
+            <b-button @click="valueUp(data.item)"
+                      :disabled="isDisabled(data.item)"
+                      variant="success" size="sm">
+              <b-icon-arrow-up-short></b-icon-arrow-up-short>
+            </b-button>
+            <b-button @click="valueDown(data.item)"
+                      :disabled="data.item.number === 8"
+                      variant="danger" size="sm">
+              <b-icon-arrow-down-short></b-icon-arrow-down-short>
+            </b-button>
+          </div>
+        </template>
+
+        <template #cell(raceBonus)="data">
+          <div v-if="selectedRace && raceBonuses">
+            <div v-if="raceBonuses.bonusStats.includes(data.item.name)" class="d-inline-grid text-center">
+              <span class="h1">{{ data.item.raceBonus }}</span>
+            </div>
+          </div>
+        </template>
+
+        <template #head(raceBonusBtn)="data">
+          <div v-if="selectedRace && raceBonuses">
+            ({{ raceBonusPoints }})
+          </div>
+          <div v-else></div>
+        </template>
+
+        <template #cell(raceBonusBtn)="data">
+          <div v-if="selectedRace && raceBonuses">
+            <div v-if="raceBonuses.bonusStats.includes(data.item.name)" class="d-inline-grid text-center">
+              <b-button @click="raceBonusUp(data.item)"
+                        :disabled="raceBonusPoints <= 0 || data.item.raceBonus >= 1"
+                        variant="success" size="sm">
+                <b-icon-arrow-up-short></b-icon-arrow-up-short>
+              </b-button>
+              <b-button @click="raceBonusDown(data.item)"
+                        :disabled="data.item.raceBonus === 0"
+                        variant="danger" size="sm">
+                <b-icon-arrow-down-short></b-icon-arrow-down-short>
+              </b-button>
+            </div>
+          </div>
+        </template>
+
+        <template #head(classBonus)="data">
+          <div v-if="selectedRace && classBonuses">
+            Class Bonus
+          </div>
+        </template>
+
+        <template #cell(classBonus)="data">
+          <div v-if="selectedClass && classBonuses">
+            <div v-if="classBonuses.includes(data.item.name)" class="d-inline-grid text-center">
+              <span class="h1">{{ data.item.classBonus }}</span>
+            </div>
+          </div>
+        </template>
+
+        <template #head(classBonusBtn)="data">
+          <div v-if="selectedRace && classBonuses">
+            ({{ classBonusPoints }})
+          </div>
+          <div v-else></div>
+        </template>
+
+        <template #cell(classBonusBtn)="data">
+          <div v-if="selectedClass && classBonuses">
+            <div v-if="classBonuses.includes(data.item.name)" class="d-inline-grid text-center">
+              <b-button @click="classBonusUp(data.item)"
+                        :disabled="classBonusPoints <= 0 || data.item.classBonus >= 2"
+                        variant="success" size="sm">
+                <b-icon-arrow-up-short></b-icon-arrow-up-short>
+              </b-button>
+              <b-button @click="classBonusDown(data.item)"
+                        :disabled="data.item.classBonus === 0"
+                        variant="danger" size="sm">
+                <b-icon-arrow-down-short></b-icon-arrow-down-short>
+              </b-button>
+            </div>
+          </div>
+        </template>
+
+        <template #cell(totalScore)="data">
+          <span class="h1 text-center">{{ getTotal(data.item) }}</span>
+        </template>
+
+        <template #cell(modifier)="data">
+          <span class="h3 text-center">{{ getModifier(data.item) }}</span>
+        </template>
+
+      </b-table>
     </div>
 
-    <div class="row point-buy">
-      <div class="h3 col-sm-12 pl-0 pt-4">PointBuy points: {{ pointBuy }}</div>
-      <b-card-group deck class="col-sm-12">
-        <div class="text-center mt-4 col-sm-2 px-0" v-for="stat in stats">
-          <b-button @click="valueUp(stat)" :disabled="isDisabled(stat)" variant="success" size="lg">
-            <b-icon-arrow-up-short></b-icon-arrow-up-short>
-          </b-button>
-          <b-card :header="stat.name" :key="stat.id" class="text-center my-2">
-            <b-card-text class="h1">{{ stat.number + stat.raceBonus + stat.classBonus }}</b-card-text>
-          </b-card>
-          <b-button @click="valueDown(stat)" :disabled="stat.number === 8" variant="danger" size="lg">
-            <b-icon-arrow-down-short></b-icon-arrow-down-short>
-          </b-button>
-        </div>
-      </b-card-group>
-    </div>
   </div>
 </template>
 
@@ -112,6 +161,43 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
+      fields: [{
+        key: "name",
+        thClass: "align-middle text-left",
+        tdClass: "align-middle text-left"
+      }, {
+        key: "number",
+        thClass: "align-middle text-right",
+        tdClass: "align-middle text-right"
+      }, {
+        key: "numberBtn",
+        thClass: "align-middle text-left",
+        tdClass: "align-middle text-left"
+      }, {
+        key: "raceBonus",
+        thClass: "align-middle text-right",
+        tdClass: "align-middle text-right"
+      }, {
+        key: "raceBonusBtn",
+        thClass: "align-middle text-left",
+        tdClass: "align-middle text-left"
+      }, {
+        key: "classBonus",
+        thClass: "align-middle text-right",
+        tdClass: "align-middle text-right"
+      }, {
+        key: "classBonusBtn",
+        thClass: "align-middle text-left",
+        tdClass: "align-middle text-left"
+      }, {
+        key: "totalScore",
+        thClass: "align-middle text-center",
+        tdClass: "align-middle text-center"
+      }, {
+        key: "modifier",
+        thClass: "align-middle text-center",
+        tdClass: "align-middle text-center"
+      }],
       stats: [],
       races: [] as any,
       classes: [] as any,
@@ -238,7 +324,37 @@ export default Vue.extend({
     classBonusDown(stat: any) {
       stat.classBonus--;
       this.classBonusPoints++;
+    },
+    getTotal(stat: any) {
+      if (stat) {
+        return (stat.number + stat.raceBonus + stat.classBonus);
+      }
+      return 8;
+    },
+    getModifier(stat: any) {
+      if (stat) {
+        const total = this.getTotal(stat);
+        if (total <= 9) {
+          return "-1";
+        } else if (total <= 11) {
+          return "0";
+        } else if (total <= 13) {
+          return "+1";
+        } else if (total <= 15) {
+          return "+2";
+        } else if (total <= 17) {
+          return "+3";
+        } else if (total === 18) {
+          return "+4";
+        }
+      }
+      return -1;
     }
   }
 })
 </script>
+<style>
+.d-inline-grid {
+  display: inline-grid;
+}
+</style>
